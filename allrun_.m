4cb0fd1b01773ac9bbfa9a8acf762a4c999de934
gongouveia <68733294+gongouveia@users.noreply.github.com>
2020-07-30 16:12:45 +0100

    função que faz data do file

--- allrun_.m
+++ allrun_.m
@@ -7,29 +7,35 @@ clear all, close all
 
 filenames = string(importdata('.\HV_Scan\helper.txt'));
 
-
-
-
 %% 2.script para organizar os ficheiros
 
 
 clc
 
-%"[numero do evento    file   numero_de_eventos outlayers distancia_média  ]"
+%
 for file = filenames
 
-var(find(file==filenames)).num = find(file==filenames);  
-var(find(file==filenames)).name = file;
-var(find(file==filenames)).events = num_of_events(file);
+var(find(file==filenames)).num = find(file==filenames);       %numero do evento
+var(find(file==filenames)).name = file;                       %nome do evento
+var(find(file==filenames)).events = num_of_events(file);      %numero de eventos selecionados
+                                                              %alterar esta
+                                                              %função como
+                                                              %filtro
+var(find(file==filenames)).medpos  = sum(dist(file))/length(dist(file)); %media das distâncias 
+var(find(file==filenames)).maxpos = max(dist(file));                     %dist media
+[year,month,day] = monthday(file);
+var(find(file==filenames)).year = year;     %year
+var(find(file==filenames)).month= month;    %month 
+var(find(file==filenames)).day = day;       %event day
 
 
 
-
-if var(find(file==filenames)).events>50
-         var(find(file==filenames)).quality=1;
-else
-    var(find(file==filenames)).quality=0;
-end
+% 
+% if var(find(file==filenames)).events>50
+%          var(find(file==filenames)).quality=1;
+% else
+%     var(find(file==filenames)).quality=0;
+% end
 
 
 %var(find()file==filename)).dist   = 
@@ -57,9 +63,8 @@ end
 
 Theta1 = [];phi1 =[];
 for i = filenames
+    
     [Theta,phi]=Plot_Angle_Dist_function(i) 
-    
-    
     Theta1=[Theta1 ;Theta];disp(length(Theta1))
     phi1= [phi1 phi];disp(length(phi1)) 
     
